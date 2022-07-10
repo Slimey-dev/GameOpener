08fb46013c583bfc4ca2822d5117d237ee701656
Slimey-dev <djakub10@seznam.cz>
2022-07-10 11:51:56 +0200

    Added dynamic list of games from shortcut folder

--- GameOpener.py
+++ GameOpener.py
@@ -9,11 +9,28 @@ def on_quit():
     
 image = Image.open("icon.png")
 
-menu = (
-    # you can add more items for more games
-    item('Game of your choice', lambda: os.startfile("C:\Path\to\that\file\that_file.exe")),
-    item('Quit', on_quit)
-    )
+# path to that folder with your game shortcuts
+dir_path = r'D:\\Path\\To\\That\\Folder'
+
+#list with that stored files
+res = []
+
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        # add it to the list
+        res.append(os.path.basename(path).split('.')[0])
+        
+
+menu = []
+count = 0
+# create menu with all files
+for x in res:
+    menu.append(item(x,lambda icon, item: os.startfile(dir_path + "\\" + item.text)))
+    count += 1
+        
+menu.append(item("Quit", on_quit))
+
 
 icon = pystray.Icon("GameOpener", image, "GameOpener", menu)
 icon.run()
